e8910fa9e5ecd6e2f5becd185a73a671dc5dd145
Hisanobu Ogawa <ogawa-hisanobu@av.avex.co.jp>
2016-11-05 16:18:26 +0000

    10_基本課題修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105151503) do
+ActiveRecord::Schema.define(version: 20161105161156) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
